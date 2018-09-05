f243c8873375ed52e7f24cdc5fc8a8587eb3c288
cpele <geekarist@users.noreply.github.com>
2018-09-05 08:29:42 +0200

    Perform auth request, get response

--- appauth-android_codelab_init/app/src/main/java/com/google/codelabs/appauth/MainActivity.java
+++ appauth-android_codelab_init/app/src/main/java/com/google/codelabs/appauth/MainActivity.java
@@ -14,8 +14,10 @@
 
 package com.google.codelabs.appauth;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -27,21 +29,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import net.openid.appauth.AuthState;
+import net.openid.appauth.AuthorizationRequest;
 import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
 import net.openid.appauth.TokenResponse;
 import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     private static final String SHARED_PREFERENCES_NAME = "AuthStatePreference";
     private static final String AUTH_STATE = "AUTH_STATE";
     private static final String USED_INTENT = "USED_INTENT";
-
+    
     MainApplication mMainApplication;
-
+    
     // state
     AuthState mAuthState;
-
+    
     // views
     AppCompatButton mAuthorize;
     AppCompatButton mMakeApiCall;
@@ -50,26 +54,26 @@ public class MainActivity extends AppCompatActivity {
     AppCompatTextView mFamilyName;
     AppCompatTextView mFullName;
     ImageView mProfileView;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMainApplication = (MainApplication) getApplication();
-        mAuthorize = (AppCompatButton) findViewById(R.id.authorize);
-        mMakeApiCall = (AppCompatButton) findViewById(R.id.makeApiCall);
-        mSignOut = (AppCompatButton) findViewById(R.id.signOut);
-        mGivenName = (AppCompatTextView) findViewById(R.id.givenName);
-        mFamilyName = (AppCompatTextView) findViewById(R.id.familyName);
-        mFullName = (AppCompatTextView) findViewById(R.id.fullName);
-        mProfileView = (ImageView) findViewById(R.id.profileImage);
-
+        mAuthorize = findViewById(R.id.authorize);
+        mMakeApiCall = findViewById(R.id.makeApiCall);
+        mSignOut = findViewById(R.id.signOut);
+        mGivenName = findViewById(R.id.givenName);
+        mFamilyName = findViewById(R.id.familyName);
+        mFullName = findViewById(R.id.fullName);
+        mProfileView = findViewById(R.id.profileImage);
+        
         enablePostAuthorizationFlows();
-
+        
         // wire click listeners
         mAuthorize.setOnClickListener(new AuthorizeListener());
     }
-
+    
     private void enablePostAuthorizationFlows() {
         mAuthState = restoreAuthState();
         if (mAuthState != null && mAuthState.isAuthorized()) {
@@ -88,33 +92,33 @@ public class MainActivity extends AppCompatActivity {
             mSignOut.setVisibility(View.GONE);
         }
     }
-
+    
     /**
      * Exchanges the code, for the {@link TokenResponse}.
      *
      * @param intent represents the {@link Intent} from the Custom Tabs or the System Browser.
      */
     private void handleAuthorizationResponse(@NonNull Intent intent) {
-
+        
         // code from the step 'Handle the Authorization Response' goes here.
-
+        
     }
-
+    
     private void persistAuthState(@NonNull AuthState authState) {
         getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE).edit()
                                                                            .putString(AUTH_STATE,
                                                                                       authState.jsonSerializeString())
-                                                                           .commit();
+                                                                           .apply();
         enablePostAuthorizationFlows();
     }
-
+    
     private void clearAuthState() {
         getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
             .edit()
             .remove(AUTH_STATE)
             .apply();
     }
-
+    
     @Nullable
     private AuthState restoreAuthState() {
         String jsonString = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
@@ -128,29 +132,46 @@ public class MainActivity extends AppCompatActivity {
         }
         return null;
     }
-
+    
     /**
      * Kicks off the authorization flow.
      */
     public static class AuthorizeListener implements Button.OnClickListener {
-
+        
         @Override
         public void onClick(View view) {
-
-            // code from the step 'Create the Authorization Request',
-            // and the step 'Perform the Authorization Request' goes here.
-
+            
+            AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(
+                Uri.parse("https://accounts.google.com/o/oauth2/v2/auth"),
+                Uri.parse("https://www.googleapis.com/oauth2/v4/token")
+            );
+            
+            Uri redirectUri = Uri.parse("com.google.codelabs.appauth:/oauth2callback");
+            
+            AuthorizationRequest request = new AuthorizationRequest.Builder(
+                config,
+                "511828570984-fuprh0cm7665emlne3rnf9pk34kkn86s.apps.googleusercontent.com",
+                "code",
+                redirectUri
+            ).setScopes("profile")
+             .build();
+            
+            Context context = view.getContext().getApplicationContext();
+            AuthorizationService service = new AuthorizationService(context);
+            Intent intent = new Intent("com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE");
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, request.hashCode(), intent, 0);
+            service.performAuthorizationRequest(request, pendingIntent);
         }
     }
-
+    
     public static class SignOutListener implements Button.OnClickListener {
-
+        
         private final MainActivity mMainActivity;
-
-        public SignOutListener(@NonNull MainActivity mainActivity) {
+        
+        SignOutListener(@NonNull MainActivity mainActivity) {
             mMainActivity = mainActivity;
         }
-
+        
         @Override
         public void onClick(View view) {
             mMainActivity.mAuthState = null;
@@ -158,26 +179,26 @@ public class MainActivity extends AppCompatActivity {
             mMainActivity.enablePostAuthorizationFlows();
         }
     }
-
+    
     public static class MakeApiCallListener implements Button.OnClickListener {
-
+        
         private final MainActivity mMainActivity;
         private AuthState mAuthState;
         private AuthorizationService mAuthorizationService;
-
-        public MakeApiCallListener(@NonNull MainActivity mainActivity,
-                                   @NonNull AuthState authState,
-                                   @NonNull AuthorizationService authorizationService) {
+        
+        MakeApiCallListener(@NonNull MainActivity mainActivity,
+                            @NonNull AuthState authState,
+                            @NonNull AuthorizationService authorizationService) {
             mMainActivity = mainActivity;
             mAuthState = authState;
             mAuthorizationService = authorizationService;
         }
-
+        
         @Override
         public void onClick(View view) {
-
+            
             // code from the section 'Making API Calls' goes here
-
+            
         }
     }
 }
